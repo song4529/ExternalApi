d8f4143be3cdd089e33b187f565ff4aa8cd99637
song4529 <gusdn4529@gmail.com>
2024-02-28 23:59:16 +0900

    faet : UpbitApiApplication 구현

--- src/main/java/com/example/upbitApi/UpbitApiApplication.java
+++ src/main/java/com/example/upbitApi/UpbitApiApplication.java
@@ -1,5 +1,10 @@
 package com.example.upbitApi;
 
+import org.springframework.boot.SpringApplication;
+
 public class UpbitApiApplication
 {
+    public static void main(String[] args) {
+        SpringApplication.run(UpbitApiApplication.class);
+    }
 }

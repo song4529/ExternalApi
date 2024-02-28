a0d732d18a4b76928f7e49f62d4c4262bd5ca063
song4529 <gusdn4529@gmail.com>
2024-02-28 23:58:19 +0900

    faet : UpbitTicker 구현

---
+++ src/main/java/com/example/upbitApi/UpbitTicker.java
@@ -0,0 +1,28 @@
+package com.example.upbitApi;
+
+public class UpbitTicker {
+    private String market;
+    private String trade_date;
+    private String trade_time;
+    private String trade_timestamp;
+    private String opening_price;
+    private String high_price;
+    private String low_price;
+    private String trade_price;
+    private String prev_closing_price;
+    private String change;
+    private String change_price;
+    private String change_rate;
+    private String signed_change_price;
+    private String signed_change_rate;
+    private String trade_volume;
+    private String acc_trade_price;
+    private String acc_trade_price_24h;
+    private String acc_trade_volume;
+    private String acc_trade_volume_24h;
+    private String highest_52_week_price;
+    private String highest_52_week_date;
+    private String lowest_52_week_price;
+    private String lowest_52_week_date;
+    private String timestamp;
+}

adcb8a388a3891d3839779006027da0f4385fe83
song4529 <gusdn4529@gmail.com>
2024-03-03 22:50:08 +0900

    faet : 컨트롤러 필요 애너테이션 추가

--- src/main/java/com/example/movieApi/MovieApiController.java
+++ src/main/java/com/example/movieApi/MovieApiController.java
@@ -1,4 +1,9 @@
 package com.example.movieApi;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
 public class MovieApiController {
 }

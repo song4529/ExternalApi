84dd4c2790ad74e78a6af295a8c6400df478689e
song4529 <gusdn4529@gmail.com>
2024-03-09 23:37:06 +0900

    faet : MovieApiDto 구현

--- src/main/java/com/example/movieApi/MovieApiDto.java
+++ src/main/java/com/example/movieApi/MovieApiDto.java
@@ -1,4 +1,13 @@
 package com.example.movieApi;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 public class MovieApiDto {
+    @Getter
+    @AllArgsConstructor
+    public static class Response {
+        private long movieId;
+        private String title;
+    }
 }
